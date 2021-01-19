259c94d77e4be09e841aa83db2a242a9619e3899
Geoff Nelson <geoffreysnelson@gmail.com>
2021-01-19 14:16:45 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+const allSections = document.querySelectorAll('section')
+
+const subLinks = document.querySelectorAll('a.subLink')
+
+console.log(document.getElementById('sl-architecture'))
+
+console.log(subLinks)
+setAllSectionsHidden()
+
+setSectionVisible("architecture")
+
+setEventListener()
+
+
+//Set all Sections as Hidden
+function setAllSectionsHidden() {
+    allSections.forEach(section => {
+        section.style.display = "none"
+    })
+}
+
+function setSectionVisible(sectionName) {
+    allSections.forEach(section => {
+        if (section.id === sectionName) {
+            section.style.display = ""
+        }
+    })
+}
+
+function setEventListener (){
+    
+    subLinks.forEach(sublink => {
+        let linkElement = document.getElementById(sublink.id)
+        linkElement.addEventListener("click", (e) => {
+            e.preventDefault();
+            setOnClick(linkElement.id.replace('sl-', ''))
+        })
+        console.log(linkElement.getEventListeners())
+        console.log("set listener" + sublink.id)
+    })
+    
+}
+
+function setOnClick(sectionLink){
+    setAllSectionsHidden()
+    setSectionVisible(sectionLink)
+}    
